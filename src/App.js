f5338a5dae31929cbf388e6e46ecdcaa8b9ddacf
ChadDiaz <chadcdiaz@gmail.com>
2021-02-19 22:59:26 -0500

    working on Login.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, NavLink, Route, Switch } from "react-router-dom";
+import PrivateRoute from './components/PrivateRoute';
+import BubblePage from './components/BubblePage';
 
 import Login from "./components/Login";
 import "./styles.scss";
@@ -8,7 +10,14 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Route exact path="/" component={Login} />
+        <NavLink to='/login'>Log In</NavLink>
+        <Switch>
+          <Route path="/login" component={Login} />
+          <PrivateRoute path='/protected'>
+            <BubblePage />
+          </PrivateRoute>
+        </Switch>
+        
       </div>
     </Router>
   );
